bedf041e1232c9bd934c6b5655e87dcd38a1b9b9
vialaky <vialaky@gmail.com>
2024-04-02 11:51:00 +0300

    Return random word from the list

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import random
+from random import choice
 
-with open('word_list.txt', 'r') as file:
-    word_list = [line.strip() for line in file]
+
+def get_word():
+    """
+    Reads a list of words from a text file and selects one word from it randomly.
+    """
+    with open('word_list.txt', 'r') as file:
+        word_list = [line.strip() for line in file]
+    return choice(word_list)
+
+
+word = get_word()
+print(word)
